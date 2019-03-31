f88d71789fe18f8a02908586e44534c88b4d1c58
Luan Castheloge <luanccp@gmail.com>
2019-03-31 13:06:03 -0300

    Utilizando bundle

---
+++ exercicios_webpack/ex.1/logger.js
@@ -0,0 +1,6 @@
+function info(text) {
+    //usar com concatenação pra funcionar
+    console.log('INFO: ${text}')
+}
+
+module.exports = { info }
--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,1 +1,1 @@
-console.log("Luanzin")
+require('./duvidaCruel')
