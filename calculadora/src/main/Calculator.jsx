b41b7b02717b8a754bde0be051be5a5c0e696faa
Luan Castheloge <luanccp@gmail.com>
2019-04-18 18:17:12 -0300

    Display de resultados estilizados

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -2,6 +2,7 @@ import React, {Component } from 'react'
 import './Calculator.css'
 
 import Button from '../components/Button'
+import Display from '../components/Display'
 
 export default class Calculator extends Component{
 
@@ -9,6 +10,7 @@ export default class Calculator extends Component{
     {
         return(
             <div className="calculator">
+                <Display value={100}></Display>
                 <Button label='AC'></Button>
                 <Button label='0'></Button>
                 <Button label='1'></Button>
