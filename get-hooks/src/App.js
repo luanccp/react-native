5d11288e8dac963958675a224de2c9b95ff56767
Luan Castheloge <luanccp@gmail.com>
2019-05-09 17:23:44 -0300

    pegando repo do github e favoritando

---
+++ get-hooks/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/luanccp/repos');
+      const data = await response.json();
+      setRepositories(data);
+    }
+    fetchData();
+
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `Você tem ${filtered.length} repositorios favoritos`
+  }, [repositories]) //toda vez que os repositories alterar este hook é ativado
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
+  return (
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>Favorito!</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
