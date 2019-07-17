8484077f12b26871af274d4d96a6818ce7341c18
Luan Castheloge <luanccp@gmail.com>
2019-07-17 10:56:16 -0300

    [STYLES] Desing login page done

--- react-native-firebase/App.js
+++ react-native-firebase/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Item, Form, Input, Button, Label } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container  style={styles.container}>
+        <Form>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input autoCapitalize="none" autoCorrect={false} />
+          </Item>
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </Item>
+          <Button full rounded>
+            <Text>SignIn</Text>
+          </Button>
+        </Form>
+      </Container>
   );
 }
 
@@ -13,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
 });
